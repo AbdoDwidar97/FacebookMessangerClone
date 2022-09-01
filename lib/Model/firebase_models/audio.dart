7bf2bc1b1fdc2dff8fa8a8735a08e00fe6b26970
a.dwidar <wfzaoaonisjavltvkyay6qtlqia6xoguus6e3lidplsdydv4puta>
2022-09-01 15:32:21 +0200

    Firebase functionality added for chatting (realtime database).

---
+++ lib/Model/firebase_models/audio.dart
@@ -0,0 +1,28 @@
+class Audio
+{
+  Audio({
+    this.name,
+    this.size,
+    this.type,
+    this.url,
+  });
+
+  String? name;
+  int? size;
+  String? type;
+  String? url;
+
+  factory Audio.fromJson(Map<String, dynamic> json) => Audio(
+    name: json["name"],
+    size: json["size"],
+    type: json["type"],
+    url: json["url"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "size": size,
+    "type": type,
+    "url": url,
+  };
+}
